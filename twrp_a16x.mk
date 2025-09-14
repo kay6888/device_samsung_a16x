cb02f06483c1ff526216f47221debcb34a7aaee7
kay6888 <kaynikko88@gmail.com>
2025-09-14 01:10:00 -0500

    Add files via upload

--- twrp_a16x.mk
+++ twrp_a16x.mk
@@ -1,30 +1,29 @@
 #
-# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
-# Release name
-PRODUCT_RELEASE_NAME := a16x
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit device configuration
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from a16x device
 $(call inherit-product, device/samsung/a16x/device.mk)
 
-## Device identifier. This must come after all inclusions
-PRODUCT_NAME := twrp_a16x
 PRODUCT_DEVICE := a16x
-PRODUCT_MODEL := SM-A166u1
+PRODUCT_NAME := twrp_a16x
 PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A166U
 PRODUCT_MANUFACTURER := samsung
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/a16x/recovery/root,recovery/root)
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="a16xsq-user 13 TP1A.220624.014 A166USQS1AYA3 release-keys"
+
+BUILD_FINGERPRINT := samsung/a16xsq/a16x:13/TP1A.220624.014/A166USQS1AYA3:user/release-keys
