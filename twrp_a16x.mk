9659d3b1564063ff0c8604a0d1cd806c21e04f28
kay6888 <kaynikko88@gmail.com>
2025-09-15 04:26:52 -0500

    Update and rename twrp_a166u1.mk to twrp_a16x.mk

---
+++ twrp_a16x.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := a16x
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/a16x/device.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := twrp_a16x
+PRODUCT_DEVICE := a16x
+PRODUCT_MODEL := SM-A166u1
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/a16x/recovery/root,recovery/root)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="a16xsq-user 13 TP1A.220624.014 A166USQS1AYA3 release-keys"
+
+BUILD_FINGERPRINT := samsung/a16xsq/a16x:13/TP1A.220624.014/A166USQS1AYA3:user/release-keys
