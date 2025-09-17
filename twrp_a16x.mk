33d0a645be359d32fa65310de1c199d981a0415b
kay6888 <kaynikko88@gmail.com>
2025-09-17 06:38:14 -0500

    Update twrp_a16x.mk

--- twrp_a16x.mk
+++ twrp_a16x.mk
@@ -4,8 +4,9 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
 # Inherit from common tree
-include device/samsung/s5e8535-common/BoardConfigCommon.mk
+$(call inherit-product, $(device/samsung/s5e8535-common/BoardConfigCommon.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
