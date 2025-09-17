1de2fc86bdd31babf3d354cb3d054ff7b62895dd
kay6888 <kaynikko88@gmail.com>
2025-09-16 22:38:15 -0500

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,7 @@ TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/recovery_dtbo
 
 # Inherit from common tree
-include device/samsung/s5e8535-common/BoardConfigCommon.mk
+Inherit device/samsung/s5e8535-common/BoardConfigCommon.mk
 
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
