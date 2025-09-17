48d67f79fbe8d1688eb8010d5838ddb2b642d01f
kay6888 <kaynikko88@gmail.com>
2025-09-16 22:42:15 -0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,12 +1,39 @@
 #
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 LOCAL_PATH := device/samsung/a16x
 
+# API levels
+PRODUCT_SHIPPING_API_LEVEL := 30
 
-# Inherit from common tree
-#include device/samsung/s5e8535-common/twrp_s5e8535.mk
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.1-impl-mock \
+    fastbootd 
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 34
